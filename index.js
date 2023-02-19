760deac363c89222ca0d96570ab8efc4a0e01d3e
kaushit tejwani <tejwanikaushit@gmail.com>
2023-02-19 16:32:37 +0530

    Revert "inital commit"
    This reverts commit b8ab440f7f6f5e7a4ad5cd14b28a97fcc4482eb5.

--- index.js
+++
@@ -1,9 +0,0 @@
-
-//firebase connectivity
-
-var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
-admin.initializeApp({
-credential: admin.credential.cert(serviceAccount),
-databaseURL: "https://fir-project-1-58a04.firebaseio.com"
-});
