a5b2a94fb66884a1ea3eb7926df1d8419b30a70c
kaushit tejwani <tejwanikaushit@gmail.com>
2023-02-27 21:38:53 +0530

    merged

---
+++ verification.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const users = require('./model/users')
+const nodemailer=require('nodemailer')
+const jwt=require('jsonwebtoken')
+
+router.get('/verification/:token', async (req,res)=>{
+    const token =req.params.token
+    const decode = jwt.decode(token, { complete: true })
+    const userid=decode.payload._id
+    const User= await users.findOne({_id:userid})
+    if(User){
+       const user=  await users.updateOne({verified:false},{$set:{verified:true}})
+       res.status(200).json({
+            message:"user verified successfully",
+            success:true,
+            user:{
+                username:User.username,
+                email:User.email,
+                password:User.password,
+                verified:true,
+            }
+
+        })
+       
+    }else{
+        return res.status(403).send({
+            message:"user verified unsuccessfully",
+            success:false,
+            error:{
+                   message:"user not registerd",
+                   verified:false
+            }
+        })
+    }
+
+
+  
+})
+module.exports=router
