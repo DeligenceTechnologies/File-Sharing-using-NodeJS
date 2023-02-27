d4c84d73c35f131fd8bfdfcb2a18ecc177ad4ce0
kaushit tejwani <tejwanikaushit@gmail.com>
2023-02-27 18:06:22 +0530

    email validation

---
+++ model/users.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema
+const bcrypt = require('bcryptjs');
+const { boolean } = require('joi');
+
+const fileSchema = new schema({
+    username: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+    },
+    verified:{
+        type:Boolean,
+        default:false
+
+    }
+}, { timestamps: true }
+)
+fileSchema.pre("save", async function (next) {
+    // const passwordHash= await bcrypt.hash(req.body.password,10);
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10)
+
+        // const passwordCompare=await bcrypt.compare(req.body.password,req.body.confirmPassword)
+    }
+    next();
+
+})
+module.exports = mongoose.model('user-authentication', fileSchema);
