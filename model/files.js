21c700e6a30921ba1800007d80c8869b5494abd0
kaushit tejwani <tejwanikaushit@gmail.com>
2023-02-27 21:33:26 +0530

    merge commit

--- model/files.js
+++ model/files.js
@@ -4,6 +4,7 @@ const schema= mongoose.Schema
 const fileSchema=new schema({
   username:{
     type:String,
+<<<<<<< HEAD
    
   },
   email:{
@@ -23,6 +24,42 @@ fileSchema.pre("save", async function(next){
   
   // const passwordCompare=await bcrypt.compare(req.body.password,req.body.confirmPassword);
 
+=======
+    default:null
+  },
+  email:{
+    type:String,
+    unique:true,
+    default:null
+},
+password:{
+    type:String,
+    default:null
+},
+confirmPassword:{
+    type:String,
+    default:null
+},
+  filename:{
+   type:String,
+   default:null
+    },
+    path:{type:String,default:null},
+    size:{type:Number,default:null},
+    uuid:{type:String,default:null},
+    sender:{type:String,default:null},
+    reciever:{type:String,default:null}
+},{timestamps:true}
+)
+fileSchema.pre("save", async function(next){
+  // const passwordHash= await bcrypt.hash(req.body.password,10);
+   if(this.isModified("password")){
+  this.password= await  bcrypt.hash(this.password,10)
+  this.confirmPassword=await bcrypt.hash(this.confirmPassword,10)
+  
+  // const passwordCompare=await bcrypt.compare(req.body.password,req.body.confirmPassword);
+
+>>>>>>> parent of c78ac36 (email validation)
 }
   next();
   
