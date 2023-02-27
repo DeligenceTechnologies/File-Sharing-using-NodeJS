9433b6ce8f7e3f4c576f74c118d0825046c6279f
kaushit tejwani <tejwanikaushit@gmail.com>
2023-02-27 21:31:05 +0530

    email validation

--- model/files.js
+++ model/files.js
@@ -1,20 +1,30 @@
-const mongoose = require("mongoose")
-const bcrypt = require('bcryptjs');
-const schema = mongoose.Schema
-const fileSchema = new schema({
-  userId: {
-    type: String,
-    required: true
+const  mongoose = require("mongoose")
+const bcrypt=require('bcryptjs');
+const schema= mongoose.Schema
+const fileSchema=new schema({
+  username:{
+    type:String,
+   
   },
-  filename: {
-    type: String,
-    required: true
-  },
-  url: {
-    type: String,
-    required: true
-  }
-}, { timestamps: true }
-)
+  email:{
+    type:String,
 
-module.exports = mongoose.model('file', fileSchema);
+},
+password:{
+    type:String,
+},
+},{timestamps:true}
+)
+fileSchema.pre("save", async function(next){
+  // const passwordHash= await bcrypt.hash(req.body.password,10);
+   if(this.isModified("password")){
+  this.password= await  bcrypt.hash(this.password,10)
+  this.confirmPassword=await bcrypt.hash(this.confirmPassword,10)
+  
+  // const passwordCompare=await bcrypt.compare(req.body.password,req.body.confirmPassword);
+
+}
+  next();
+  
+})
+module.exports=mongoose.model('file',fileSchema);
