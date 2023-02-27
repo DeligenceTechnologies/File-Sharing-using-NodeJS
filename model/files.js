f605946ee680a32f0c60093fc46dc1fe19f8af15
kaushit tejwani <tejwanikaushit@gmail.com>
2023-02-27 17:37:25 +0530

    Revert "email validation"
    This reverts commit c78ac360445777cf1ae595a3cdf9f7a60994b7fc.

--- model/files.js
+++ model/files.js
@@ -1,20 +1,45 @@
-const mongoose = require("mongoose")
-const bcrypt = require('bcryptjs');
-const schema = mongoose.Schema
-const fileSchema = new schema({
-  userId: {
-    type: String,
-    required: true
+const  mongoose = require("mongoose")
+const bcrypt=require('bcryptjs');
+const schema= mongoose.Schema
+const fileSchema=new schema({
+  username:{
+    type:String,
+    default:null
   },
-  filename: {
-    type: String,
-    required: true
-  },
-  url: {
-    type: String,
-    required: true
-  }
-}, { timestamps: true }
+  email:{
+    type:String,
+    unique:true,
+    default:null
+},
+password:{
+    type:String,
+    default:null
+},
+confirmPassword:{
+    type:String,
+    default:null
+},
+  filename:{
+   type:String,
+   default:null
+    },
+    path:{type:String,default:null},
+    size:{type:Number,default:null},
+    uuid:{type:String,default:null},
+    sender:{type:String,default:null},
+    reciever:{type:String,default:null}
+},{timestamps:true}
 )
+fileSchema.pre("save", async function(next){
+  // const passwordHash= await bcrypt.hash(req.body.password,10);
+   if(this.isModified("password")){
+  this.password= await  bcrypt.hash(this.password,10)
+  this.confirmPassword=await bcrypt.hash(this.confirmPassword,10)
+  
+  // const passwordCompare=await bcrypt.compare(req.body.password,req.body.confirmPassword);
 
-module.exports = mongoose.model('file', fileSchema);
+}
+  next();
+  
+})
+module.exports=mongoose.model('file',fileSchema);
--- signup.js
+++ signup.js
@@ -3,7 +3,6 @@ const users = require('./model/users')
 const bcrypt = require('bcryptjs');
 const legit = require('legit');
 const jwt = require('jsonwebtoken');
-const nodemailer=require('nodemailer')
 
 
 //validation error using joi module
@@ -12,34 +11,20 @@ const joi = require('joi')
 const signupSchema = joi.object({
   username: joi.string().required(),
   email: joi.string().email().required(),
-  password: joi.string().required(),
+  password: joi.string().required()
 })
-
-
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_host,
-  port: process.env.SMTP_PORT,
-  secure: false,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  }
-});
 
 router.post('/', async (req, res) => {
   async function signup() {
-   // const { emailFrom } = req.body;
-
     const { error, value } = signupSchema.validate(req.body, { abortEarly: false })
     if (error) {
       return res.status(402).json(error.details)
     }
 
-    const user = await users.findOne({ $or:[{username: req.body.username},{email: req.body.email}]})
+    const user = await users.findOne({ username: req.body.username, email: req.body.email })
     if (user) {
-      return res.status(403).send("the usernamee or either email is already exist")
+      return res.status(403).send("the user is already exist,please try another username & email")
     }
-   
     else {
 
       await legit(req.body.email)
@@ -60,18 +45,16 @@ router.post('/', async (req, res) => {
               email: user.email,
             }
             const token = jwt.sign(payload, "rANDOMSTRIGN", { expiresIn: "300000000000000000000" })
-            const mailOptions = {
-
-              from:"emailverification@darty.fr.cr",
-              to: req.body.email,
-             subject:"verification email",
-             text:"http://localhost:4000/verification/"+token
-            };
-      
-            await transporter.sendMail(mailOptions);
             res.status(201).json({
-              message: "the verification email was successfully sent",
+              message: "user successfully registerd",
               success: true,
+              user: {
+                username: user.username,
+                email: user.email,
+                password: user.password,
+                user_id: user._id,
+                token: "Bearer " + token,
+              }
             }
             )
 
