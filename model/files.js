239651498ecb600de3fa8f002b5720336807dea3
kaushit tejwani <tejwanikaushit@gmail.com>
2023-02-27 21:44:41 +0530

    merged

--- model/files.js
+++ model/files.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose")
 const bcrypt = require('bcryptjs');
 const schema = mongoose.Schema
@@ -6,15 +5,6 @@ const fileSchema = new schema({
   userId: {
     type: String,
     required: true
-=======
-const  mongoose = require("mongoose")
-const bcrypt=require('bcryptjs');
-const schema= mongoose.Schema
-const fileSchema=new schema({
-  username:{
-    type:String,
-   
->>>>>>> 61152fd81162d75b387e7584f7c3b470616bf952
   },
   filename: {
     type: String,
@@ -27,13 +17,4 @@ const fileSchema=new schema({
 }, { timestamps: true }
 )
 
-<<<<<<< HEAD
 module.exports = mongoose.model('file', fileSchema);
-=======
->>>>>>> parent of c78ac36 (email validation)
-}
-  next();
-  
-})
-module.exports=mongoose.model('file',fileSchema);
->>>>>>> 61152fd81162d75b387e7584f7c3b470616bf952
