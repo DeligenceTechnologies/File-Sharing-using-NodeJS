9fc2dd61b42ae752e44a6742735d4dd829dded61
kaushittejwani01 <95282960+kaushittejwani@users.noreply.github.com>
2023-03-03 13:19:43 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,7 +2,26 @@ const  mongoose = require("mongoose");
 mongoose.set('strictQuery', true)
 require('dotenv').config();
 function db(){
-    mongoose.connect("mongodb+srv://kaushit:"+encodeURIComponent("7H$y9!E.vX#qg#Q")+"@cluster1.4ve1duv.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true});
+    mongoose.connect("var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'secret';
+opts.issuer = 'accounts.examplesoft.com';
+opts.audience = 'yoursite.net';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));",{useNewUrlParser:true,useUnifiedTopology:true});
     const connection=mongoose.connection
     connection.once('open', function () {
         console.log('MongoDB running');
